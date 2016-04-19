39b281b934fda24c611e4c2520c271d0aae78774
Frederik Vantroys <frederik.vantroys@realdolmen.com>
2016-04-19 11:09:14 +0200

    'APRJTWO-2'

---
+++ fleet/src/main/java/com/realdolmen/fleet/config/ResourceConfig.java
@@ -0,0 +1,50 @@
+package com.realdolmen.fleet.config;
+
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class ResourceConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+
+        String[] strBaseNames = {
+                "classpath:locales/rdfmessages_global",
+        };
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames(strBaseNames);
+        messageSource.setUseCodeAsDefaultMessage(true);
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(10);
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+
+        LocaleChangeInterceptor result = new LocaleChangeInterceptor();
+        result.setParamName("lang");
+
+        return result;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+
+        return sessionLocaleResolver;
+    }
+
+
+}
