2b0ce49a152091226e2c03a948107f5584bbceee
Frederik Vantroys <frederik.vantroys@realdolmen.com>
2016-04-21 11:56:57 +0200

    APRJTWO-20

---
+++ fleet/src/main/java/com/realdolmen/fleet/controller/admin/EditCarsController.java
@@ -0,0 +1,38 @@
+package com.realdolmen.fleet.controller.admin;
+
+import com.realdolmen.fleet.domain.Car;
+import com.realdolmen.fleet.repositories.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin/cars")
+public class EditCarsController {
+
+    @Autowired
+    private CarRepository carRepository;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String carcatalog(Model model) {
+        List<Car> adminCars = carRepository.findAll();
+        model.addAttribute("cars", adminCars);
+        return "admin/allcars";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String getCarDetail(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("car", carRepository.findOne(id));
+        return "admin/caredit";
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public String mycar() {
+        return "admin/createcar";
+    }
+}
