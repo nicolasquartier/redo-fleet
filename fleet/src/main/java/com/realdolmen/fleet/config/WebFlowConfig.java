7c7c41c18d9eb419e1b5e12c976c5ff94e983fc0
Nicolas Quartier <nicolasquartier@msn.com>
2016-04-21 16:23:16 +0200

    Part of APRJTWO-15 implemented, but not done yet! APRJTWO-18: Done

---
+++ fleet/src/main/java/com/realdolmen/fleet/config/WebFlowConfig.java
@@ -0,0 +1,7 @@
+package com.realdolmen.fleet.config;
+
+/**
+ * Created by NQRAZ66 on 21/04/2016.
+ */
+public class WebFlowConfig {
+}
---
+++ fleet/src/main/java/com/realdolmen/fleet/config/WebMvcConfig.java
@@ -0,0 +1,54 @@
+package com.realdolmen.fleet.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.webflow.mvc.servlet.FlowHandlerAdapter;
+import org.springframework.webflow.mvc.servlet.FlowHandlerMapping;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.AjaxThymeleafViewResolver;
+import org.thymeleaf.spring4.view.FlowAjaxThymeleafView;
+
+@Configuration
+@Profile(value = {"PRD", "TST"})
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/index").setViewName("index");
+//        registry.addViewController("/").setViewName("index");
+//        registry.addViewController("/login").setViewName("login");
+//    }
+
+    @Autowired
+    private WebFlowConfig webFlowConfig;
+
+    @Autowired
+    private SpringTemplateEngine springTemplateEngine;
+
+    @Bean
+    public FlowHandlerMapping flowHandlerMapping() {
+        FlowHandlerMapping handlerMapping = new FlowHandlerMapping();
+        handlerMapping.setOrder(-1);
+        handlerMapping.setFlowRegistry(this.webFlowConfig.flowRegistry());
+        return handlerMapping;
+    }
+
+    @Bean
+    public FlowHandlerAdapter flowHandlerAdapter() {
+        FlowHandlerAdapter handlerAdapter = new FlowHandlerAdapter();
+        handlerAdapter.setFlowExecutor(this.webFlowConfig.flowExecutor());
+        handlerAdapter.setSaveOutputToFlashScopeOnRedirect(true);
+        return handlerAdapter;
+    }
+
+    @Bean
+    public AjaxThymeleafViewResolver ajaxThymeleafViewResolver() {
+        AjaxThymeleafViewResolver viewResolver = new AjaxThymeleafViewResolver();
+        viewResolver.setViewClass(FlowAjaxThymeleafView.class);
+        viewResolver.setTemplateEngine(springTemplateEngine);
+        return viewResolver;
+    }
+}
