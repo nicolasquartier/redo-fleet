61ae0c9f1a4442788d610c83dddc2602d703872b
Reinout Claeys <reinout.claeys@realdolmen.com>
2016-04-22 09:51:32 +0200

    attempting to fix travis #4

--- fleet/src/test/java/com/realdolmen/fleet/flow/OrderCompanyCarFlowTest.java
+++ fleet/src/test/java/com/realdolmen/fleet/flow/OrderCompanyCarFlowTest.java
@@ -26,7 +26,7 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class OrderCompanyCarFlow extends AbstractXmlFlowExecutionTests {
+public class OrderCompanyCarFlowTest extends AbstractXmlFlowExecutionTests {
 
     @Mock
     private CarRepository carRepository;
