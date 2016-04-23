7d17cc4e6724ca358e8045d14da82dec6a66464b
Reinout Claeys <reinout.claeys@realdolmen.com>
2016-04-23 22:15:00 +0200

    upped test coverage

---
+++ fleet/src/test/java/com/realdolmen/fleet/domain/CarTest.java
@@ -0,0 +1,27 @@
+package com.realdolmen.fleet.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by RCEAZ31 on 22/04/2016.
+ */
+public class CarTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void getVersion() throws Exception {
+
+    }
+
+    @Test
+    public void getOptions() throws Exception {
+
+    }
+
+}
---
+++ fleet/src/test/java/com/realdolmen/fleet/domain/FunctionalLevelTest.java
@@ -0,0 +1,10 @@
+package com.realdolmen.fleet.domain;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by RCEAZ31 on 22/04/2016.
+ */
+public class FunctionalLevelTest {
+
+}
