c24ea08e918eaefabd56ae4307e92647441807e5
Reinout Claeys <reinout.claeys@realdolmen.com>
2016-04-26 13:37:28 +0200

    test bugfix attempt : changing transacional isolation level

--- fleet/src/test/java/com/realdolmen/fleet/SpringBootTransactionalIntegrationTest.java
+++ fleet/src/test/java/com/realdolmen/fleet/SpringBootTransactionalIntegrationTest.java
@@ -3,6 +3,7 @@ package com.realdolmen.fleet;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.lang.annotation.Retention;
@@ -11,7 +12,7 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 @ContextConfiguration(classes = TestConfig.class)
 @TestPropertySource(locations = "/test.properties")
-@Transactional
+@Transactional(isolation = Isolation.READ_COMMITTED)
 @ActiveProfiles("TST")
 public @interface SpringBootTransactionalIntegrationTest {
 
